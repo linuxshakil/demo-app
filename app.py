e210eca3fe7fe75074dcffa8cc3347e13f60b42e
Shakil <linux.shakil@gmail.com>
2026-08-02 05:51:34 +0000

    update: Code for Demo app

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ APP_VERSION = os.environ.get("APP_VERSION", "dev")
 @app.route("/")
 def index():
     return jsonify(
-        message="Hello from Shakil's demo-app from Seprate reop!",
+        message="Hello from Shakil's demo-app from Demo Seprate !",
         environment=ENVIRONMENT,
         version=APP_VERSION,
     )
